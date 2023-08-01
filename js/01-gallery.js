ed674376f24e8037df9806f4ab0d029838b90a66
Serhii120796 <sfedyura@ukr.net>
2023-08-01 22:40:52 +0300

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,25 +3,46 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
-gallery.innerHTML = galleryItems
-  .map(
+gallery.addEventListener('click', onClick);
+
+gallery.insertAdjacentHTML('beforeend', createMarcup(galleryItems));
+
+function createMarcup(arr) {
+return arr.map(
     ({ preview, original, description }) =>
       `<li class="gallery__item">
         <a class="gallery__link" href="${original}">
             <img class="gallery__image"
-                src="${preview}" 
-                data-source="${original}"
-                alt="${description}"/>
+              src="${preview}" 
+              data-source="${original}"
+              alt="${description}"/>
         </a>
        </li>`,
   )
   .join('');
-
-gallery.addEventListener('click', onClick);
-
-function onClick(evt) {
-    evt.target.preventDefault;
-    console.log(evt.target);
 }
 
+function onClick(evt) {
+if (evt.target === evt.currentTarget) {
+    return;
+}
+  
+  evt.preventDefault();
 
+  const currentLink = evt.target.closest('.gallery__link');
+  
+  const modalWindow = basicLightbox.create(`
+    <img src="${currentLink.href}" width="800" height="600">
+`)
+  modalWindow.show()
+  
+ document.addEventListener('keydown', closeModalWindow);
+
+  function closeModalWindow(evt) {
+  if (evt.code === "Escape") {
+    modalWindow.close();
+    }
+  }
+}
+  
+ 
